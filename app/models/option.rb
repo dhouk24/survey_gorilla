c6ae257d91bdace6a15fb980c95d149088027cc2
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-24 17:41:01 -0500

    debugged survey_stats.erb, debugged users.erb, updated Option model

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,8 +1,5 @@
 class Option < ActiveRecord::Base
   belongs_to :question
-  belongs_to :survey, :through => :question
-  has_many   :responses 
-  has_many   :users, :through => :responses
 
   def add_response!
     self.count += 1
