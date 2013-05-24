3fdb3daf08fa23e9793206a969e6e478a6969a0b
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-24 13:26:33 -0500

    comments for routes

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -24,3 +24,18 @@ end
 post '/login' do
   
 end
+
+get '/create' do
+  erb :create_survey
+end
+
+post '/create' do
+  #inital: make new Survey object, store id, save object as instance variable?
+  #updates survey creation view
+  #takes in new question data, makes new Question object, Options objects
+  #adds question object to questions array (array = instance variable)
+  #adds options objects to options array
+  erb :create_survey
+end
+
+# publishing survey => Survey.create, Question.create, Option.create
