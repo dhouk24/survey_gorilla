1564878c8f635cd900ab2b2baf84cfd3c655387e
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-24 18:18:47 -0500

    Start of javascript handling

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,24 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+
+
+
+
+
+
+
+
+//
+  $("#add-player").click(function(event){
+    event.preventDefault();
+    var num = $('form div').length + 1;
+    $('form div:last').append( newForm(num) );
+    $('form div:last').hide().slideDown('medium');
+  });
+
+function newForm(num) {
+  string = "<div>\
+      <input type='text' name='players["+num+"][name]' placeholder=\"Enter Player "+num+"'s initials\"> \
+      <input class='keyboard' type='text' name='players["+num+"][code]' placeholder='key'>\
+      </div>";
+  return string;
+};
