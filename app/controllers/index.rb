555c0e38c9c9b05265e1cfb54faa33a3275d93f9
dbc-apprentice <apprentice@devbootcamp.com>
2013-05-24 13:01:16 -0500

    Basic login and signup pages

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,25 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/login' do
+
+erb :login
+
+end
+
+get '/signup' do
+
+end
+
+post '/signup' do
+
+end
+
+post '/logout' do
+
+end
+
+post '/login' do
+  
+end
